1d027886463f1626d8352adde9e56bea78b4c04a
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-27 16:28:03 +0100

    done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = true;
+    var task2 = false;
   }
+  var task = false;
+  var task2 = true;
 
   return [task, task2];
 }
