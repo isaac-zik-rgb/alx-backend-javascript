302b2dcbc3a24a3bf8ea69799776b5463db83b66
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-27 22:25:54 +0100

    done

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,12 @@
 export default function createIteratorObject(report) {
-    //This function will return an iterator to go through every employee in every department.
-    const employee = []
-    for (const key in report.allEmployees) {
-        employee.push(...report.allEmployees[key])
-    }
-    return employee
+  // This function will return an iterator to go through every employee in every department.
+  const employees = [];
 
+  for (const key in report.allEmployees) {
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, key)) {
+      employees.push(...report.allEmployees[key]);
+    }
+  }
+
+  return employees;
 }
