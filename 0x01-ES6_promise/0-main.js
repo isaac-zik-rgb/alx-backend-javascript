d5866c07c8c2630489d3192842b2aab61e0bea9f
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-29 09:14:31 +0100

    task2

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -5,8 +5,14 @@ import getResponseFromAPI from "./0-promise.js";
 const response = getResponseFromAPI();
 
 console.log(response instanceof Promise);
-*/
+
 import getFullResponseFromAPI from './1-promise';
 
 console.log(getFullResponseFromAPI(true));
 console.log(getFullResponseFromAPI(false));
+*/
+
+import handleResponseFromAPI from "./2-then";
+
+const promise = Promise.resolve();
+handleResponseFromAPI(promise);
---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,12 @@
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  }).catch((error) => {
+    console.error('Error from the API:', error);
+    throw error;
+  });
+}
