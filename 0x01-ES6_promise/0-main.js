858eaa11f194594c15c467e578534daf164216bc
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-29 12:30:08 +0100

    task 6

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -24,8 +24,12 @@ handleProfileSignup();
 import signUpUser from "./4-user-promise";
 
 console.log(signUpUser("Bob", "Dylan"));
-*/
+
 
 import uploadPhoto from './5-photo-reject';
 
 console.log(uploadPhoto('guillaume.jpg'));
+*/
+import handleProfileSignup from './6-final-user';
+
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+  return Promise.allSettled(promise).then((result) => result.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
+}
