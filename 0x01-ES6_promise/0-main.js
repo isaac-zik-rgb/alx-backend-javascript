0f5f4481dd32ae1eed8e424c1b3a5c388754ee17
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-29 11:39:03 +0100

    task4

--- 0x01-ES6_promise/0-main.js
+++ 0x01-ES6_promise/0-main.js
@@ -16,7 +16,11 @@ import handleResponseFromAPI from "./2-then";
 
 const promise = Promise.resolve();
 handleResponseFromAPI(promise);
-*/
+
 import handleProfileSignup from "./3-all";
 
 handleProfileSignup();
+*/
+import signUpUser from "./4-user-promise";
+
+console.log(signUpUser("Bob", "Dylan"));
