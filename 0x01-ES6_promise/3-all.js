24a80f015b88e6b2f4630c545516e101019f6414
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-29 18:22:40 +0100

    promise.all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return uploadPhoto().then((usermessage) => createUser().then((message) => {
-    console.log(usermessage.body, message.firstName, message.lastName);
-  }).catch('Signup system offline')).catch('Signup system offline');
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoMessage, userMessage]) => {
+      console.log(photoMessage.body, userMessage.firstName, userMessage.lastName);
+    })
+    .catch(() => {
+      console.error('Signup system offline');
+    });
 }
