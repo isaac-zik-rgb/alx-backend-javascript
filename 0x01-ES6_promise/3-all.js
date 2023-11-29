4ca2142b8de9a97da55e99f87cf57e8383d09c38
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-29 15:51:53 +0100

    Refactor handleProfileSignup function

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,5 @@
 import { uploadPhoto, createUser } from './utils';
 
-/* export  default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoMessage, userMessage]) => {
-      const profile = photoMessage.body;
-      const userFullName = `${userMessage.firstName} ${userMessage.lastName}`;
-      console.log(profile, userFullName);
-    }).catch(() => {
-      console.error('Signup system offline');
-    });
-}
-*/
 export default function handleProfileSignup() {
   return uploadPhoto().then((usermessage) => createUser().then((message) => {
     console.log(usermessage.body, message.firstName, message.lastName);
