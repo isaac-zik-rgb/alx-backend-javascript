54e1f6312363a0c091f60049d7a05d231ec9171d
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-29 11:18:35 +0100

    save

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
+function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photoMessage, userMessage]) => {
       const profile = photoMessage.body;
