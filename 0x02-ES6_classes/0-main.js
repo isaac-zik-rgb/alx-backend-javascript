a1782c789b0df6a9abd480c7013f5a31e898d466
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-30 15:44:48 +0100

    Add code to initialize classrooms

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -1,4 +1,10 @@
+/*
 import ClassRoom from "./0-classroom.js";
 
 const room = new ClassRoom(10);
 console.log(room._maxStudentsSize)
+*/
+
+import initializeRooms from './1-make_classrooms.js';
+
+console.log(initializeRooms());
