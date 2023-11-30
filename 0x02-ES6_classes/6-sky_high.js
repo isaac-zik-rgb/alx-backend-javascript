0aa876db4d15a8229021c03f8e7041403e08f542
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-30 18:22:27 +0100

    Refactor SkyHighBuilding class in 6-sky_high.js

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,18 +1,20 @@
-import Building from "./5-building";
+import Building from './5-building';
 
-export default class SkyHighBuilding extends Building{
-    constructor(_sqft, floors){
-        super(_sqft);   
-        this._floors = floors;
-    }
-    get floors(){
-        return this._floors;
-    }
-    set floors(floors){
-        this._floors = floors;
-    }
-    evacuationWarningMessage(){
-        return `Evacuate slowly the ${this._floors} floors`;
-    }
-    
+export default class SkyHighBuilding extends Building {
+  constructor(_sqft, floors) {
+    super(_sqft);
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
