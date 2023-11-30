56ff28d0c03c5a5f703b681f4ac0c1568a589554
isaac castro <isaacokechukwu20021@gmail.com>
2023-11-30 18:26:43 +0100

    Add SkyHighBuilding and Airport classes

--- 0x02-ES6_classes/0-main.js
+++ 0x02-ES6_classes/0-main.js
@@ -57,10 +57,16 @@ try {
 catch(err) {
     console.log(err);
 }
-*/
+
 import SkyHighBuilding from './6-sky_high.js';
 
 const building = new SkyHighBuilding(140, 60);
 console.log(building.sqft);
 console.log(building.floors);
 console.log(building.evacuationWarningMessage());
+*/
+import Airport from "./7-airport.js";
+
+const airportSF = new Airport('San Francisco Airport', 'SFO');
+console.log(airportSF);
+console.log(airportSF.toString());
