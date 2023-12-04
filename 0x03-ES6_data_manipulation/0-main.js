2ffb45f623697c33f78f77275cd02df9ffaf0d19
isaac castro <isaacokechukwu20021@gmail.com>
2023-12-04 19:49:43 +0100

    Add getStudentIdsSum function

--- 0x03-ES6_data_manipulation/0-main.js
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -8,9 +8,16 @@ import getListStudents from "./0-get_list_students.js";
 
 console.log(getListStudentIds("hello"));
 console.log(getListStudentIds(getListStudents()));
-*/
+
 import getListStudents from "./0-get_list_students.js";
 import getStudentsByLocation from "./2-get_students_by_loc.js";
 
 const students = getListStudents();
 console.log(getStudentsByLocation(students, 'San Francisco'));
+*/
+import getListStudents from "./0-get_list_students.js";
+import getStudentIdsSum from "./3-get_ids_sum.js";
+
+const students = getListStudents();
+const value = getStudentIdsSum(students);
+console.log(value);
