c8c51e9d687aa7d327fbc01986abebf2bc50e442
isaac castro <isaacokechukwu20021@gmail.com>
2023-12-04 09:26:13 +0100

    Add import statements and console.log statements for testing getListStudentIds function

--- 0x03-ES6_data_manipulation0x03-ES6_data_manipulation/0-main.js
+++ 0x03-ES6_data_manipulation0x03-ES6_data_manipulation/0-main.js
@@ -1,3 +1,10 @@
+/*
 import getListStudents from "./0-get_list_students.js";
 
 console.log(getListStudents());
+*/
+import getListStudentIds from "./1-get_list_student_ids.js";
+import getListStudents from "./0-get_list_students.js";
+
+console.log(getListStudentIds("hello"));
+console.log(getListStudentIds(getListStudents()));
