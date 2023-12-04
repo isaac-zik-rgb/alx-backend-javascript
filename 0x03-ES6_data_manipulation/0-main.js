92b25ed263c7e89db9182fa9a3d325290e2bf7d8
isaac castro <isaacokechukwu20021@gmail.com>
2023-12-04 20:13:12 +0100

    Add setFromArray function

--- 0x03-ES6_data_manipulation/0-main.js
+++ 0x03-ES6_data_manipulation/0-main.js
@@ -28,7 +28,11 @@ import updateStudentGradeByCity from "./4-update_grade_by_city.js";
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }, { studentId: 1, grade: 86 }]));
 
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 5, grade: 97 }]));
-*/
+
 import createInt8TypedArray from "./5-typed_arrays.js";
 
 console.log(createInt8TypedArray(10, 2, 89));
+*/
+import setFromArray from "./6-set.js";
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
