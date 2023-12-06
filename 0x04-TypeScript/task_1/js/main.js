dbdebb8dcf82899a08d21176bd7a0d92d32a8d77
isaac castro <isaacokechukwu20021@gmail.com>
2023-12-06 17:28:26 +0100

    Refactor printTeacher function

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,8 +1,4 @@
-var director1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    location: 'London',
-    fullTimeEmployee: true,
-    numberOfReports: 17,
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName.charAt(0), ". ").concat(lastName);
 };
-console.log(director1);
+console.log(printTeacher("John", "Doe"));
