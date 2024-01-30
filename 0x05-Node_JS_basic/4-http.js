5399ab160f722176ff37536d88480ce22b4be53a
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-01-30 22:33:27 +0100

    Update module.exports in 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,8 +7,7 @@ const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
-}
-);
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
