e980ace7c3ef3917d310d3fd62dac4e5d6aa658f
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-02-01 19:51:56 +0100

    Refactor API tests to use request library

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,15 +1,27 @@
-const request = require('supertest');
-const app = require('./api');
-const { expect } = require('chai');
+const request = require("request");
+const expect = require("chai").expect;
 
-describe('GET /', () => {
-    it('checks for response', (done) => {
-        request(app)
-            .get('/')
-            .expect(200, 'Welcome to the payment system')
-            .end((err, res) => {
-                if (err) throw err;
-                done();
-            });
+describe("Index page", function() {
+    const options = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("check correct content", function(done) {
+	request(options, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    });
+    it("check correct content length", function(done) {
+	request(options, function(err, res, body) {
+	    expect(res.headers['content-length']).to.equal('29');
+	    done();
+	});
     });
 });
