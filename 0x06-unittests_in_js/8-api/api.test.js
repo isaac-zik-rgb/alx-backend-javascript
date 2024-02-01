d6b31dac4e465094528546aca2b806f0957a2131
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-02-01 20:03:06 +0100

    Add unit tests for API endpoints

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -24,4 +24,41 @@ describe("Index page", function() {
 	    done();
 	});
     });
+
+});
+
+describe("Cart page", function() {
+    const options = {
+    url: "http://localhost:7865/cart/12",
+    method: "GET"
+    }
+    it("check correct status code", function(done) {
+    request(options, function(err, res, body) {
+        expect(res.statusCode).to.equal(200);
+        done();
+    });
+    });
+    it("check correct content", function(done) {
+    request(options, function(err, res, body) {
+        expect(body).to.contain("Payment methods for cart 12");
+        done();
+    });
+    });
+    it("check correct content length", function(done) {
+    request(options, function(err, res, body) {
+        expect(res.headers['content-length']).to.equal('32');
+        done();
+    });
+    });
+    it("checks for correct staus code for invalid cart id", function(done) {
+        const options = {
+        url: "http://localhost:7865/cart/hello",
+        method: "GET"
+        }
+        request(options, function(err, res, body) {
+            expect(res.statusCode).to.equal(404);
+            done();
+        });
+    });
+
 });
