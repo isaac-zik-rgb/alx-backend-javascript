9d28f61a4214e4b33fa0b60d90cfa7b3414f38a2
Isaac Okechukwu Chukwuma <wwwdavidcastro565@gmail.com>
2024-02-01 20:06:37 +0100

    Add Mocha test framework and update API tests

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,4 +1,5 @@
 const request = require("request");
+const {describe, it} = require("mocha");
 const expect = require("chai").expect;
 
 describe("Index page", function() {
@@ -14,51 +15,29 @@ describe("Index page", function() {
     });
     it("check correct content", function(done) {
 	request(options, function(err, res, body) {
-	    expect(body).to.contain("Welcome to the payment system");
+	    expect(body).to.equal("Welcome to the payment system");
 	    done();
 	});
     });
-    it("check correct content length", function(done) {
-	request(options, function(err, res, body) {
-	    expect(res.headers['content-length']).to.equal('29');
-	    done();
-	});
-    });
-
 });
 
 describe("Cart page", function() {
-    const options = {
-    url: "http://localhost:7865/cart/12",
-    method: "GET"
-    }
-    it("check correct status code", function(done) {
-    request(options, function(err, res, body) {
-        expect(res.statusCode).to.equal(200);
-        done();
+    it("check correct status code for correct url", function(done) {
+	request.get("http://localhost:7865/cart/12", function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
     });
+    it("check correct content for correct url", function(done) {
+	request.get("http://localhost:7865/cart/12", function(err, res, body) {
+	    expect(body).to.contain("Payment methods for cart 12");
+	    done();
+	});
     });
-    it("check correct content", function(done) {
-    request(options, function(err, res, body) {
-        expect(body).to.contain("Payment methods for cart 12");
-        done();
+    it("check correct status code for incorrect url", function(done) {
+	request.get("http://localhost:7865/cart/kim", function(err, res, body) {
+	    expect(res.statusCode).to.equal(404);
+	    done();
+	});
     });
-    });
-    it("check correct content length", function(done) {
-    request(options, function(err, res, body) {
-        expect(res.headers['content-length']).to.equal('32');
-        done();
-    });
-    });
-    it("checks for correct staus code for invalid cart id", function(done) {
-        const options = {
-        url: "http://localhost:7865/cart/hello",
-        method: "GET"
-        }
-        request(options, function(err, res, body) {
-            expect(res.statusCode).to.equal(404);
-            done();
-        });
-    });
-
 });
